5a8153a3067df9fffc561ff70a34689a1f98b9bc
ghigbie <gehigbie@gmail.com>
2019-11-19 20:01:09 -0700

    Add methods

---
+++ src/main/java/georgehigbie/com/starterapi/ui/controller/UserController.java
@@ -0,0 +1,29 @@
+package georgehigbie.com.starterapi.ui.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping
+    public String  getUser(){
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete user was called";
+    }
+
+}
