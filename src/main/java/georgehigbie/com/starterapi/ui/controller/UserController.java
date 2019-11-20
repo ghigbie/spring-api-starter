daf137c2ae0ddc2076041c402661504dc7fdee1d
ghigbie <gehigbie@gmail.com>
2019-11-19 20:34:00 -0700

    Add new class

--- src/main/java/georgehigbie/com/starterapi/ui/controller/UserController.java
+++ src/main/java/georgehigbie/com/starterapi/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package georgehigbie.com.starterapi.ui.controller;
 
+import georgehigbie.com.starterapi.ui.models.request.UserDetailsRequestModel;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser(){
+    public String createUser(@RequestBody UserDetailsRequestModel userDetails){
         return "create user was called";
     }
 
