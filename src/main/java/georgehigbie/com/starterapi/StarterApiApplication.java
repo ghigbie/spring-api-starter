42a35245f2b1f4b9e4775e0d0ef7bf91c5db4377
ghigbie <gehigbie@gmail.com>
2019-11-19 19:51:31 -0700

    Initial commit

---
+++ src/main/java/georgehigbie/com/starterapi/StarterApiApplication.java
@@ -0,0 +1,13 @@
+package georgehigbie.com.starterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/georgehigbie/com/starterapi/StarterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package georgehigbie.com.starterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
